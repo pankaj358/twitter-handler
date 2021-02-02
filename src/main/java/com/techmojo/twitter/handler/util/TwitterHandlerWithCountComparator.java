c99e491ab232673b2626a85350e20b673c2d1870
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-02-03 01:47:36 +0530

    Inverted the comparator condition

--- src/main/java/com/techmojo/twitter/handler/util/TwitterHandlerWithCountComparator.java
+++ src/main/java/com/techmojo/twitter/handler/util/TwitterHandlerWithCountComparator.java
@@ -8,9 +8,9 @@ public class TwitterHandlerWithCountComparator implements Comparator<TwitterHand
     @Override
     public int compare(TwitterHandlerWithCount tweetWithCount1, TwitterHandlerWithCount tweetWithCount2) {
 
-        return tweetWithCount1.count - tweetWithCount2.count == 0
-                ? tweetWithCount1.handlerName.compareTo(tweetWithCount2.handlerName)
-                : tweetWithCount1.count - tweetWithCount2.count;
+        return  tweetWithCount1.count.equals(tweetWithCount2.count) ?
+                 tweetWithCount1.handlerName.compareTo(tweetWithCount2.handlerName)
+                : tweetWithCount2.count.compareTo(tweetWithCount1.count);
 
     }
 }
